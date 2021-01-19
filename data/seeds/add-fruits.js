b88bfa00619ddbf556cfa2499fd52152e0bd743f
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-19 19:57:08 +0100

    wip

--- data/seeds/add-fruits.js
+++ data/seeds/add-fruits.js
@@ -1,13 +1,13 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('fruits').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
+      return knex('fruits').insert([
+        { name: 'rowValue1',  },
+        { name: 'rowValue2',  },
+        { name: 'rowValue3',  },
       ]);
     });
 };
