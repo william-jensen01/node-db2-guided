fe1072138c8998908d48d0fcddee3b283ea173fb
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-19 19:29:37 +0100

    wip

--- data/migrations/20210119192020_make-fruits-table.js
+++ data/migrations/20210119192020_make-fruits-table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   // make the fruits table
   return knex.schema.createTable(table => {
     // add primary key unique not null 'id' (integer)
-    table.increments();
+    table.increments('frui');
 
     // add not null, unique 'name' (varchar 128)
     table.string('name', 128).notNullable().unique();
