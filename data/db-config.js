d6fad784a652844e8c90c27d9344941119e3da75
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-19 18:48:43 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -5,11 +5,3 @@ const configs = require('../knexfile.js');
 const db = knex(configs[env]);
 
 module.exports = db;
-
-// const db = knex({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: './data/produce.db3'
-//   },
-//   useNullAsDefault: true
-// });
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,10 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/produce.db3',
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
